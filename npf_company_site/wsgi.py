1e76950aefe7e71dea74bbfa732161e4eeb89912
konstantin <kuznetsovkm@yandex.ru>
2022-05-08 19:48:10 +0300

    init_commit

---
+++ npf_company_site/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for npf_company_site project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'npf_company_site.settings')
+
+application = get_wsgi_application()
